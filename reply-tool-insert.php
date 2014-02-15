bfcc8ffcde626717638ca2162b2637a6d88477a0
ScottSmith <ScottSmith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-15 05:56:38 +0000

    Tagging version 1.1.6
    git-svn-id: https://plugins.svn.wordpress.org/decode-reply-tool/tags/1.1.6@858297 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ reply-tool-insert.php
@@ -0,0 +1,9 @@
+<div class="decode-reply-tool-plugin">
+
+	<a href="<?php echo esc_url( 'https://twitter.com/intent/tweet?screen_name=' . get_option( 'twitter-username' ) . '&text=(about%3A%20' ); echo the_permalink() . ')'; ?>" class="twitterreply replylink left" target="_blank" data-related="<?php echo get_option( 'twitter-username' ); ?>"><?php _e( 'With Twitter', 'decode-reply-tool' ); ?></a>
+
+	<div class="replytrigger"><?php _e( 'Reply', 'decode-reply-tool' ); ?></div>
+
+	<a href="<?php echo esc_url( 'https://alpha.app.net/intent/post?text=@' . get_option( 'adn-username' ) . '&nbsp;(about%3A%20' ); echo the_permalink() . ') '; ?>" class="adnreply replylink right" target="_blank"><?php _e( 'With ADN', 'decode-reply-tool' ); ?></a>
+
+</div>
