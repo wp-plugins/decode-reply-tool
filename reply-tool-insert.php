2ac7fe201cc9d3389a48bfbfc323d9c1755c34d7
ScottSmith <ScottSmith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-04 02:07:28 +0000

    Updated reply links to remove any redirects.
    git-svn-id: https://plugins.svn.wordpress.org/decode-reply-tool/trunk@959751 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- reply-tool-insert.php
+++ reply-tool-insert.php
@@ -1,9 +1,11 @@
 <div class="decode-reply-tool-plugin">
 
-	<a href="<?php echo esc_url( 'https://twitter.com/intent/tweet?screen_name=' . get_option( 'twitter-username' ) . '&text=(about%3A%20' ); echo the_permalink() . ')'; ?>" class="twitterreply replylink left" target="_blank" data-related="<?php echo get_option( 'twitter-username' ); ?>"><?php _e( 'With Twitter', 'decode-reply-tool' ); ?></a>
+	<a href="<?php echo esc_url( 'https://twitter.com/intent/tweet?screen_name=' . get_option( 'twitter-username' ) . '&text=(about%3A%20' . get_permalink() . ')%20' ); ?>" class="twitterreply replylink left" target="_blank" data-related="<?php echo get_option( 'twitter-username' ); ?>"><?php _e( 'With Twitter', 'decode-reply-tool' ); ?></a>
 
 	<div class="replytrigger"><?php _e( 'Reply', 'decode-reply-tool' ); ?></div>
 
-	<a href="<?php echo esc_url( 'https://account.app.net/intent/post?text=@' . get_option( 'adn-username' ) . '&nbsp;(about%3A%20' ); echo the_permalink() . ') '; ?>" class="adnreply replylink right" target="_blank"><?php _e( 'With ADN', 'decode-reply-tool' ); ?></a>
+	<a href="<?php echo esc_url( 'https://account.app.net/intent/post?text=@' . get_option( 'adn-username' ) . '%20(about%3A%20' . get_permalink() . ')%20' ); ?>" class="adnreply replylink right" target="_blank"><?php _e( 'With ADN', 'decode-reply-tool' ); ?></a>
+	
+	<a href="<?php echo esc_url( 'https://account.app.net/intent/post?text=@' . get_option( 'adn-username' ) . '%20(about%3A%20' . get_permalink() . ')%20' ); ?>" class="adnreply replylink right" target="_blank"><?php _e( 'With ADN', 'decode-reply-tool' ); ?></a>
 
 </div>
